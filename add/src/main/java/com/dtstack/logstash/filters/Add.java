0d72ed9957dcc98a0ce816324eb37e5f392f778c
sishu@dtstack.com <sishu@dtstack.com>
2016-11-17 17:58:32 +0800

    ysq

--- add/src/main/java/com/dtstack/logstash/filters/Add.java
+++ add/src/main/java/com/dtstack/logstash/filters/Add.java
@@ -1,6 +1,7 @@
 package com.dtstack.logstash.filters;
 
 import java.net.InetAddress;
+//import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import org.joda.time.DateTime;
@@ -36,27 +37,47 @@ public class Add extends BaseFilter {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	protected Map filter(final Map event) {
-		Set<Map.Entry<String,Object>> sets =fields.entrySet();
-		for(Map.Entry<String,Object> entry:sets){
-			String key = entry.getKey();
-			if(event.get(key)==null){
-				Object value = entry.getValue();
-				event.put(key, value);
-				if(event.get(value)!=null){
-					event.put(key, event.get(value));
-				}else if(value instanceof String){
-					String vv =value.toString();
-	        		if ("%{hostname}%".equals(vv)){
-	        			event.put(key, getHostName());
-	        		}else if("%{timestamp}%".equals(vv)){
-	        			event.put(key, getTimeStamp());
-	        		}else if("%{ip}%".equals(vv)){
-	        			event.put(key, getHostAddress());
-	        		}
-	            }
-			} 
+		try{
+			Set<Map.Entry<String,Object>> sets =fields.entrySet();
+			for(Map.Entry<String,Object> entry:sets){
+				String key = entry.getKey();
+				if(event.get(key)==null){
+					Object value = entry.getValue();
+					event.put(key, value);
+					if(event.get(value)!=null){
+						event.put(key, event.get(value));
+					}else if(value instanceof String){
+						String vv =value.toString();
+						if(vv.indexOf(".")>0){
+							String[] vs=vv.split("\\.");
+							Object oo = event;
+							for(int i=0;i<vs.length;i++){
+								oo = loopObject(vs[i],oo);
+								if(oo==null)break;	
+							}
+							if(oo!=null)event.put(key, oo);	
+						}else if ("%{hostname}%".equals(vv)){
+		        			event.put(key, getHostName());
+		        		}else if("%{timestamp}%".equals(vv)){
+		        			event.put(key, getTimeStamp());
+		        		}else if("%{ip}%".equals(vv)){
+		        			event.put(key, getHostAddress());
+		        		}
+		            }
+				} 
+			}
+		}catch(Exception e){
+			logger.error("Add filter error:{}",e.getCause());
 		}
 		return event;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Object loopObject(String value,Object obj){
+		if(obj instanceof Map){
+			return ((Map<String,Object>)obj).get(value);
+		} 
+        return null;
 	}
 	
 	public static String getHostNameForLiunx() {  
@@ -98,7 +119,18 @@ public class Add extends BaseFilter {
 		return DateTime.now().toString();
 	}	
 	
-	public static void main(String[] args){
-		getHostAddress();
-	}
+//	public static void main(String[] args){
+//		Map<String,Object> mm =new HashMap<String,Object>();
+//		mm.put("agent_type", "@metadata.beat.yy");
+//		Add.fields= mm;
+//		Add add = new Add(new HashMap<String,Object>());
+//		add.prepare();
+//		Map<String,Object> event =new HashMap<String,Object>();
+//		Map<String,Object> e1 =new HashMap<String,Object>();
+//		e1.put("type", "flow");
+//		e1.put("beat", "packetbeat");
+//		event.put("@metadata",e1);
+//		add.filter(event);
+//		System.out.println(event);
+//	}
 }
